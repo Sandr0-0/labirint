a2c3e5de0850fd0da36ba9a9892d4a32c40caaf2
Sandro <ilihaleksandr777@gmail.com>
2023-06-09 18:34:34 +0200

    sprites

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def file_path(file_name):
     folder = os.path.abspath(__file__ + "/..")
     path = os.path.join(folder, file_name)
     return path
-
+    
 WIN_WIDTH = 900
 WIN_HEIGHT = 800
 FPS = 40
@@ -29,14 +29,19 @@ class GameSprite(pygame.sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
+player = GameSprite(5, 5, 60, 80, r"image\vs2.png")
+enemy1 = GameSprite(200, 60 , 60, 60, r"image\huskar.jpg")
+goal = GameSprite(500, 60, 80, 80, r"image\tron.jpg")
+
 
 walls = pygame.sprite.Group()
-wall1 = GameSprite(100, 100, 5, 400, r"image\WALL.png")
+wall1 = GameSprite(100, 100, 20, 400, r"image\WALL.png")
 walls.add(wall1)
 wall2 = GameSprite(300, 600, 300, 20, r"image\WALL.png")
 walls.add(wall2)
-level = 1
 
+
+level = 1
 game = True
 
 while game:
@@ -47,6 +52,8 @@ while game:
     if level == 1:
         window.blit(fon, (0, 0))
         walls.draw(window)
-
+        player.show()
+        enemy1.show()
+        goal.show()
     clock.tick(FPS)
     pygame.display.update()
